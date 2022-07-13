86a0a91543f668c40eac4e15514c3f9cbac6ecef
Mahardhika Chandra Nur Ikhsan <mahardhikachandra048@gmail.com>
2022-07-14 03:34:41 +0700

    feat(test): add testing in product controller

--- test/product/addProduct.test.js
+++ test/product/addProduct.test.js
@@ -1,10 +1,14 @@
 const request = require('supertest');
 const path = require('path');
 const app = require('../../app');
+const {Products} = require('../../app/models');
 
-const image = path.resolve(__dirname, '../../docs/assets/1.jpg');
+const image1 = path.resolve(__dirname, '../../docs/assets/1.jpg');
+// const image2= path.resolve(__dirname, '../../docs/assets/2.jpg');
+// const image3 = path.resolve(__dirname, '../../docs/assets/3.jpg');
+// const image4 = path.resolve(__dirname, '../../docs/assets/4.jpg');
 
-describe('PUT /api/v1/users/addProfil', () => {
+describe('POST /api/v1/addProduct', () => {
   const userLogin = {
     email:"rizki@gmail.com",
     password:"12345"
@@ -20,17 +24,50 @@ describe('PUT /api/v1/users/addProfil', () => {
         jwtToken = res.body.token;
       });
   });
+  let product;
+  afterAll(async () => {
+    product = await Products.destroy({
+      where: {
+        nama_produk:'Redmi Note 10 Pro test',
+        harga_produk:'3999999',
+        kategori:'Hp',
+        deskripsi:'Hp bagus No frem'  
+      },
+    });
+  });
+
 it('success create product with 201 as status code', async () => {
     return await request(app)
-      .put('/api/v1/users/addProfil')
+      .post('/api/v1/addProduct')
       .set('authorization', `Bearer ${jwtToken}`)
       .set('Content-Type', 'multipart/form-data')
-      .field('kota', 'kudus')
-      .field('alamat', 'sidomulyo')
-      .field('no_hp', '087123444345')
-      .attach('foto', image)
+      .field('nama_produk', 'Redmi Note 10 Pro error')
+      .field('harga_produk', '3999999')
+      .attach('gambar', image1)
+    //   .attach('gambar', image2)
+    //   .attach('gambar', image3)
+    //   .attach('gambar', image4)
+      .field('kategori', 'hp')
+      .field('deskripsi', 'Hp bagus No frem')
       .then((res) => {
         expect(res.statusCode).toBe(201);
+      });
+  });
+  it('failed create product with 400 as status code', async () => {
+    return await request(app)
+    .post('/api/v1/addProduct')
+    .set('authorization', `Bearer ${jwtToken}`)
+    .set('Content-Type', 'multipart/form-data')
+    .field('nama_produk', '')
+    .field('harga_produk', '')
+    .attach('gambar', )
+  //   .attach('gambar', image2)
+  //   .attach('gambar', image3)
+  //   .attach('gambar', image4)
+    .field('kategori', '')
+    .field('deskripsi', '')
+      .then((res) => {
+        expect(res.statusCode).toBe(400);
       });
   });
 
