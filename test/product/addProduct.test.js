61ca846e0ff3bcb4200161c98f8fc1fadad2f5a5
Mahardhika Chandra Nur Ikhsan <mahardhikachandra048@gmail.com>
2022-07-14 14:10:34 +0700

    feat(test): add testing in product controller(add product)

--- test/product/addProduct.test.js
+++ test/product/addProduct.test.js
@@ -1,75 +1,60 @@
-const request = require('supertest');
-const path = require('path');
-const app = require('../../app');
-const {Products} = require('../../app/models');
+const request = require("supertest");
+const path = require("path");
+const app = require("../../app");
+const { Products } = require("../../app/models");
 
-const image1 = path.resolve(__dirname, '../../docs/assets/1.jpg');
-// const image2= path.resolve(__dirname, '../../docs/assets/2.jpg');
-// const image3 = path.resolve(__dirname, '../../docs/assets/3.jpg');
-// const image4 = path.resolve(__dirname, '../../docs/assets/4.jpg');
 
-describe('POST /api/v1/addProduct', () => {
-  const userLogin = {
-    email:"rizki@gmail.com",
-    password:"12345"
-  };
+
+const image1 = path.resolve(__dirname, "../../docs/assets/1.jpg");
+const image2 = path.resolve(__dirname, "../../docs/assets/2.jpg");
+const image3 = path.resolve(__dirname, '../../docs/assets/3.jpg');
+const image4 = path.resolve(__dirname, '../../docs/assets/4.jpg');
+
+describe("POST /api/v1/addProduct", () => {
 
   let jwtToken;
-  beforeEach(async () => {
-    await request(app)
+  beforeAll(async () => {
+    loginUser = await request(app)
       .post('/api/v1/users/login')
-      .set('Content-Type', 'application/json')
-      .send(userLogin)
-      .then((res) => {
-        jwtToken = res.body.token;
+      .send({
+        email: "rizki@gmail.com",
+        password: "12345",
       });
-  });
+    jwtToken = loginUser.body.token;
+    console.log(jwtToken)
+  })
+
   let product;
   afterAll(async () => {
     product = await Products.destroy({
       where: {
-        nama_produk:'Redmi Note 10 Pro test',
-        harga_produk:'3999999',
-        kategori:'Hp',
-        deskripsi:'Hp bagus No frem'  
+        nama_produk: "Redmi Note 10 Pro test",
+        harga_produk: "3999999",
+        kategori: "Hp",
+        deskripsi: "Hp bagus ",
       },
     });
   });
-
-it('success create product with 201 as status code', async () => {
-    return await request(app)
-      .post('/api/v1/addProduct')
-      .set('authorization', `Bearer ${jwtToken}`)
-      .set('Content-Type', 'multipart/form-data')
-      .field('nama_produk', 'Redmi Note 10 Pro error')
-      .field('harga_produk', '3999999')
-      .attach('gambar', image1)
-    //   .attach('gambar', image2)
-    //   .attach('gambar', image3)
-    //   .attach('gambar', image4)
-      .field('kategori', 'hp')
-      .field('deskripsi', 'Hp bagus No frem')
-      .then((res) => {
-        expect(res.statusCode).toBe(201);
-      });
-  });
-  it('failed create product with 400 as status code', async () => {
-    return await request(app)
-    .post('/api/v1/addProduct')
-    .set('authorization', `Bearer ${jwtToken}`)
-    .set('Content-Type', 'multipart/form-data')
-    .field('nama_produk', '')
-    .field('harga_produk', '')
-    .attach('gambar', )
-  //   .attach('gambar', image2)
-  //   .attach('gambar', image3)
-  //   .attach('gambar', image4)
-    .field('kategori', '')
-    .field('deskripsi', '')
-      .then((res) => {
-        expect(res.statusCode).toBe(400);
-      });
-  });
-
- 
+  it('Add a session', function (done) {
+    request(app).post('/api/v1/addProduct')
+      .set('content-type', 'application/octet-stream')
+      .set('Authorization', `Bearer ${jwtToken}`)
+       .attach("gambar",image1)
+       .attach("gambar", image2)
+       .attach('gambar', image3)
+       .attach('gambar', image4)
+       .attach({
+        nama_produk: "Redmi Note 10 Pro test",
+        harga_produk: "3999999",
+        kategori: "Hp",
+        deskripsi: "Hp bagus ",
+      })
+      .then(response => {
+        expect(response.statusCode).toBe(201);
+        done()
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
 });
