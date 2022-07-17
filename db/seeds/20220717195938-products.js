b628d2b02b3a497979a97ad4818f6742bf4174a9
Mahardhika Chandra Nur Ikhsan <mahardhikachandra048@gmail.com>
2022-07-18 03:59:33 +0700

    add seeders

--- db/seeds/20220717195938-products.js
+++ db/seeds/20220717195938-products.js
@@ -1,10 +1,4 @@
 'use strict';
-const path = require('path');
-
-const image1 = path.resolve(__dirname, '../../docs/assets/1.jpg');
-const image2 = path.resolve(__dirname, '../../docs/assets/2.jpg');
-const image3 = path.resolve(__dirname, '../../docs/assets/3.jpg');
-const image4 = path.resolve(__dirname, '../../docs/assets/4.jpg');
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -12,7 +6,7 @@ module.exports = {
       id_user: 2,
       nama_produk: 'Laptop Alienware',
       harga_produk: 30000000,
-      gambar: [image1,image2,image3,image4],
+      gambar: null,
       kategori: 'Laptop',
       deskripsi: 'Laptop Gaming',
       totalLike: 0,
@@ -24,7 +18,7 @@ module.exports = {
       id_user: 2,
       nama_produk: 'Laptop Alienware 2',
       harga_produk: 20000000,
-      gambar: [image1,image2,image3,image4],
+      gambar: null,
       kategori: 'Laptop',
       deskripsi: 'Laptop Gaming',
       totalLike: 0,
@@ -36,7 +30,7 @@ module.exports = {
       id_user: 2,
       nama_produk: 'Laptop Alienware 3',
       harga_produk: 40000000,
-      gambar: [image1,image2,image3,image4],
+      gambar: null,
       kategori: 'Laptop',
       deskripsi: 'Laptop Gaming',
       totalLike: 0,
