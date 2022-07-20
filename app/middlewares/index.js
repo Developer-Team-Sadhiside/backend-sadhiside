bbc0bd050a5b4d045e57e4dd763f24af4e0909c6
Mr. Rizki <rizkioktav70@gmail.com>
2022-07-20 20:04:34 +0700

    feat(test): add credential for check user

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -2,6 +2,7 @@ const auth = require('./authentication');
 const uploader = require('./uploader');
 const ImSeller = require('./credentialSeller');
 const ImBuyer = require('./credentialBuyer');
+const checkUser = require('./credentialCheckUser');
 const uploadOnMemory = require('./uploudOnMemory');
 
 module.exports = {
@@ -10,4 +11,5 @@ module.exports = {
   ImSeller,
   ImBuyer,
   uploadOnMemory,
+  checkUser,
 };
