b50529229c8296989f2001c066b73eb2c1c5c8a5
Mahardhika Chandra Nur Ikhsan <mahardhikachandra048@gmail.com>
2022-07-21 23:53:02 +0700

    add testing history

---
+++ test/history/rejectOffer.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const app = require('../../app');
+// const { Products } = require("../../app/models");
+
+describe('POST /api/v1/history/seller/rejectOffer/:id', () => {
+  let jwtToken;
+  beforeAll(async () => {
+    loginUser = await request(app)
+      .post('/api/v1/users/login')
+      .send({
+        email: 'rizki1@gmail.com',
+        password: '123456',
+      });
+    jwtToken = loginUser.body.token;
+  });
+
+  it('Where user success offer, user will get status 200', () => request(app)
+  .post('/api/v1/history/seller/rejectOffer/2')
+  .set('authorization', `Bearer ${jwtToken}`)
+  .set('Accept', 'application/json')
+  .then((res) => {
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        status: expect.any(String),
+        message: expect.any(String),
+      }),
+    );
+  }));
+
+});
