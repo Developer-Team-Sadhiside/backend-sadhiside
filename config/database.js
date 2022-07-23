268d635ff2144825103a138f2ce0f9c4450e5f21
Mr. Rizki <rizkioktav70@gmail.com>
2022-07-24 03:06:59 +0700

    edit database production

--- config/database.js
+++ config/database.js
@@ -40,7 +40,7 @@ module.exports = {
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: 'd5j4pe494gjrgi',
     host: DB_HOST,
     port: DB_PORT,
     dialect: 'postgres',
