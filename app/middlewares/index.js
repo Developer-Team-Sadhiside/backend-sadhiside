05c85fcf271c1bbe19d21c981d09eecc3872fdc3
Mahardhika Chandra Nur Ikhsan <mahardhikachandra048@gmail.com>
2022-07-23 15:15:17 +0700

    edit middlewares

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,15 +1,11 @@
 const auth = require('./authentication');
 const uploader = require('./uploader');
-const ImSeller = require('./credentialSeller');
-const ImBuyer = require('./credentialBuyer');
 const checkUser = require('./credentialCheckUser');
 const uploadOnMemory = require('./uploudOnMemory');
 
 module.exports = {
   auth,
   uploader,
-  ImSeller,
-  ImBuyer,
   uploadOnMemory,
   checkUser,
 };
